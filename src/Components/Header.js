67e90143aad98ff128b4a870af35f17e250987de
Harishcruise <harishbarath2001@gmail.com>
2023-05-18 19:59:13 -0400

    section-1 completed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import git from '../Assets/Github.png'
 import fig from '../Assets/Figma.png'
-import drib from '../Assets/Dribble.png'
+import linkedin from '../Assets/Linkedin.png'
 import Style from './Header.module.css'
 
-function Header() {
+function Header({children}) {
   return (
     <div style={{display:"flex"}}>
     <div className={Style.sideContainer}>
         <div className={Style.line}></div>
         <img src={git} alt='git'/>
         <img src={fig} alt='fig'/>
-        <img src={drib} alt='drib'/>
+        <img src={linkedin} alt='linkin'/>
     </div>
+    
+    <div style={{width:"100%"}}>
     <div className={Style.container}>
        <div className={Style.leftContainer}>
         <p>Harish</p>
@@ -23,6 +25,10 @@ function Header() {
         <p className={Style.btn}><span style={{color:"#C778DD",marginRight:"2px"}}>#</span>about-me</p>
         <p className={Style.btn}><span style={{color:"#C778DD",marginRight:"2px"}}>#</span>contacts</p>
        </div>
+    </div>
+    <>
+      {children}
+    </>
     </div>
     </div>
   )
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,36 @@ import Header from '../Components/Header'
 function Home() {
     return (
         <>
-            <Header/>
+            <Header>
+
+            <div className={Style.sectionOneContainer}>
+            <div className={Style.sectionOne}>
+               <div className={Style.leftContainerSecOne}>
+               
+               <div className={Style.title}>
+                  <p>
+                    Harish is a <span style={{color:"#C778DD"}}>full-stack developer</span> and <span style={{color:"#C778DD"}}>software engineer</span>.
+                  </p>
+               </div>
+               
+               <div className={Style.subTitle}>
+                  <p>
+                  He crafts responsive websites where technologies meet creativity
+                  </p>
+               </div>
+
+               <div className={Style.contactBtn}>
+                  Contact Me !!
+               </div>
+                
+               </div>
+               <div className={Style.RightContainerSecOne}>
+
+               </div>
+            </div>
+            </div>
+
+            </Header>
         </>
     )
 }
