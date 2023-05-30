84d08df151fd73fbc4c3b752bb548a6e9c4f2adb
Harishcruise <harishbarath2001@gmail.com>
2023-05-29 23:34:18 -0400

    implement initial route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import Home from './Pages/Home';
-
+import Works from './Pages/Works';
+import About from './Pages/About';
+import Contacts from './Pages/Contacts'
+import {Routes,Route} from 'react-router-dom'
 function App() {
   return (
-    <Home/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/work' element={<Works/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/contact' element={<Contacts/>} />
+    </Routes>
   );
 }
 
